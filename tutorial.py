b6b5dca1e3c1e4c5921102bdf8e0c59f2edfacaf
floating-byte <79982823+floating-byte@users.noreply.github.com>
2021-03-03 15:33:38 +0100

    how to use  : copy past kinda of code

---
+++ tutorial.py
@@ -0,0 +1,43 @@
+import openport
+
+''' u create a obj like that '''
+prox = openport.prox_master()
+
+'''setup_file() just going to create empty file'''
+''' u dont need to use it '''
+prox.setup_file("data_test")
+
+''' there is 2 web pages i can scrape for data '''
+''' https://www.sslproxies.org  -> find(1) '''
+''' https://free-proxy-list.net -> find(2) '''
+
+''' find() going to visit the web pages '''
+data = prox.find(2)
+'''
+return a json like 
+{
+	"ip":xxx.xx.xx.x,
+	"port":xx,
+	"http":True,
+	"https":True
+}
+'''
+
+''' save() just take the data and save it in a xlsx file'''
+''' save() wont save the same proxy so your data is not repeated'''
+prox.save("data_test",data)
+
+'''if u dont care about repeated data well use '''
+#prox.save_data("data_test",data)
+
+''' so u can load it using load_data() '''
+prox.load_data("data_test")
+'''
+return a json like 
+{
+	"ip":xxx.xx.xx.x,
+	"port":xx,
+	"http":True,
+	"https":True
+}
+'''
